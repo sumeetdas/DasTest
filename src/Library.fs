54c08ab080d896cd3db9c900bee47a5393887d5b
Sumeet Das <sumeetdas1992@gmail.com>
2022-02-05 09:54:04 +0530

    added support fo list and string test functions

--- src/Library.fs
+++ src/Library.fs
@@ -1,8 +1,16 @@
 namespace Das.Test
 
 module Core = 
+    type 'a TestType = 
+        | TestString of string
+        | TestList of 'a list
+
+    let _string func (x: string) = func (TestString x)
+    let _list func (x: 'z list) = func (TestList x)
+
     let is func x = func x
     let isNot func x = not (func x)
+    let has func x = func x
     let have func x = func x
     let doesNotHave func x = not (func x)
 
@@ -53,31 +61,51 @@ module Core =
             let text = sprintf "%s does not ends with %s" x text
             Error (text)
 
-    let containsString (text: string) (x: string): Result<bool, string> = 
-        if x.Contains(text) then Ok (true)
-        else 
-            let text = sprintf "%s does not contain string %s" x text
-            Error (text)
+    let containsString (text: string) (x: 'a TestType): Result<bool, string> = 
+        match x with 
+        | TestString str -> 
+            if str.Contains(text) then Ok (true)
+            else 
+                let text = sprintf "%s does not contain string %s" str text
+                Error (text)
+        | _ -> 
+            Error (sprintf "Input %A is not supported." x)
 
-    let emptyString (x:string): Result<bool, string> = 
-        if x.Length = 0 then Ok (true)
-        else 
-            let text = sprintf "%s is not an empty string" x
-            Error (text)
+    let emptyString (x: 'a TestType): Result<bool, string> = 
+        match x with 
+        | TestString str -> 
+            if str.Length = 0 then Ok (true)
+            else 
+                let text = sprintf "%s is not an empty string" str
+                Error (text)
+        | _ -> 
+            Error (sprintf "Input %A is not supported" x)
 
-    let stringLength (len: int) (x: string): Result<bool, string> = 
-        if x.Length = len then Ok (true)
-        else 
-            let text = sprintf "The length of %s is not %d" x len
-            Error (text)
+    let length (len: int) (x: 'a TestType): Result<bool, string> = 
+        match x with 
+        | TestString str -> 
+            if str.Length = len then Ok (true)
+            else 
+                let text = sprintf "The length of %A is not %d" x len
+                Error (text)
+        | TestList list -> 
+            if list.Length = len then Ok (true)
+            else 
+                let text = sprintf "The length of %A is not %d" x len
+                Error (text)
+        // | _ -> 
+        //     Error (sprintf "Input %A is not supported" x)
 
-    let listLength (len: int) (x: 'a list): Result<bool, string> = 
-        if x.Length = len then Ok (true)
-        else 
-            let text = sprintf "The length of %A is not %d" x len
-            Error (text)
-
-
+    let containsElem (elem: 'a) (x: 'a TestType): Result<bool, string> = 
+        match x with 
+        | TestList list -> 
+            if list |> List.contains elem then Ok (true)
+            else 
+                let text = sprintf "List %A does not contain not element %A" list elem
+                Error (text)
+        | _ -> 
+            Error (sprintf "This function does not support input %A" x)
+        
     let verify 
         (testName: string) 
         (testResult: Result<bool, string>)
@@ -114,7 +142,6 @@ module Core =
         : string = 
         
         let totalUnitTests = unitTestResults.Length
-        let passedUnitTests = 0
         
         let numFailedUnitTests, failedUnitTestResults =
             unitTestResults 
